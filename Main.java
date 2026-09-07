8859135ffd8bf984f4ec4c743abd6d02b4171fb6
revhan12 <revanmahardika7@gmail.com>
2026-09-07 22:42:27 +0700

    Remove Barang class implementation
    Removed Barang class and its methods from Main.java.

--- Main.java
+++ Main.java
@@ -1,26 +1,3 @@
-class Barang {
-    String nama;
-    double harga;
-    int jumlah;
-
-    Barang(String nama, double harga, int jumlah) {
-        this.nama = nama;
-        this.harga = harga;
-        this.jumlah = jumlah;
-    }
-
-    double hitungTotal() {
-        return harga * jumlah;
-    }
-
-    void tampilkanInfo() {
-        System.out.println("Nama Barang : " + nama);
-        System.out.println("Harga       : Rp" + harga);
-        System.out.println("Jumlah      : " + jumlah);
-        System.out.println("Total       : Rp" + hitungTotal());
-        System.out.println("----------------------------");
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
