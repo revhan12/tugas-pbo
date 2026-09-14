b9ee1409ce42ce8a7717f373e07dfd4d1a448abd
revhan12 <revanmahardika7@gmail.com>
2026-09-14 21:06:32 +0700

    Encapsulate fields and add getter/setter methods

--- Barang.java
+++ Barang.java
@@ -1,12 +1,44 @@
 class Barang {
-    String nama;
-    double harga;
-    int jumlah;
+    private String nama;
+    private double harga;
+    private int jumlah;
 
     Barang(String nama, double harga, int jumlah) {
         this.nama = nama;
         this.harga = harga;
         this.jumlah = jumlah;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        if (harga >= 0) {
+            this.harga = harga;
+        } else {
+            System.out.println("Harga tidak boleh negatif!");
+        }
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        if (jumlah >= 0) {
+            this.jumlah = jumlah;
+        } else {
+            System.out.println("Jumlah barang tidak boleh negatif!");
+        }
     }
 
     double hitungTotal() {
